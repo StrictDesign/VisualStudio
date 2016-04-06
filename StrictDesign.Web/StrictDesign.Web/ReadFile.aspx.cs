0bf22f5af982561b63fe08a9b7c0056ae6647587
Jimmy Mjällby <jimmy@strictdesign.se>
2016-04-06 23:09:15 +0200

    New Web project

---
+++ StrictDesign.Web/StrictDesign.Web/ReadFile.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StrictDesign.Web
+{
+    public partial class ReadFile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string fileContent = System.IO.File.ReadAllText(@"C:\Users\Jimmy\Source\Repos\VisualStudio\StrictDesign.Web\StrictDesign.Web\Text.txt");
+
+            lblInfo.Text = fileContent;
+        }
+
+        protected void btnReadFile_Click(object sender, EventArgs e)
+        {
+            //ReadFromFile content = new ReadFromFile();
+            //lblInfo.Text = fileCon
+            
+        }
+    }
+}
---
+++ StrictDesign.Web/StrictDesign.Web/ReadFromFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StrictDesign.Web
+{
+    public class ReadFromFile
+    {
+        string fileContent = System.IO.File.ReadAllText(@"C:\Users\Jimmy\Source\Repos\VisualStudio\StrictDesign.Web\StrictDesign.Web\Text.txt");
+    }
+}
